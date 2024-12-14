11b5693ef9f29f4c94aa70818d01bbbcce6d8106
Said <baraoudev@gmail.com>
2024-12-14 19:50:09 +0100

    add a listing model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, default='USD')
+    publication_date = models.DateField()
