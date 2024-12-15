e337390ac0fb5ede2557e16fadf5e2bcaa8d6d47
Said <baraoudev@gmail.com>
2024-12-15 19:13:49 +0100

    make migrations and migrate bid plus commented imagefield for the moment

--- auctions/models.py
+++ auctions/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    image = models.ImageField()
+    # image = models.ImageField()
     title = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='USD')
