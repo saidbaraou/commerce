80c9086b7e46334519518d31affcc134fd7ee8f7
Said <baraoudev@gmail.com>
2024-12-17 18:18:00 +0100

    add properties to listing and bid Models

--- auctions/models.py
+++ auctions/models.py
@@ -10,17 +10,22 @@ class Listing(models.Model):
     title = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='USD')
-    publication_date = models.DateField()
+    publication_date = models.DateField(auto_now=True)
 
     def __str__(self):
         return f"{self.title} Price: {self.price} {self.currency} {self.publication_date}"
 
 class Bid(models.Model):
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bid')
+    user = models.ForeignKey(User.first_name, on_delete=models.CASCADE, related_query_name='bid')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='USD')
 
+    class Meta:
+        ordering = ('bid_time',) # Most recent bid first
+
      def __str__(self):
-        return f"Price: {self.price} {self.currency}"
+        return f"Bid of : {self.price} {self.currency} by {self.user.first_name}"
 
 # class Comments(models.Model):
 #     return
