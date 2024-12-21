3857d00cc05464afef6a3f0054d6f97666d218ac
Said <baraoudev@gmail.com>
2024-12-21 18:46:57 +0100

    add str function to comments model

--- auctions/models.py
+++ auctions/models.py
@@ -32,3 +32,6 @@ class Comments(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='')
     text = models.TextField()
+
+    def __str__(self):
+        return f"Comment by {self.user.username}"
