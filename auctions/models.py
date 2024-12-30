07a1fdd4640a998d40cf37201e6cc8a45807efe9
Said <baraoudev@gmail.com>
2024-12-30 19:44:19 +0100

    add listing model properties

--- auctions/models.py
+++ auctions/models.py
@@ -7,13 +7,19 @@ class User(AbstractUser):
 
 class Listing(models.Model):
     title = models.CharField(max_length=64)
+    image_url = models.CharField(max_length=300, blank=True, null=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='USD')
+    publication_user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     publication_date = models.DateField(auto_now=True)
-
+    is_sold = models.BooleanField(default=False)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    
     def __str__(self):
         return f"{self.title} Price: {self.price} {self.currency} {self.publication_date}"
-    
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
