ea3b5b8394985f70020cdabf5963953aa94811d2
Said <baraoudev@gmail.com>
2024-12-30 19:54:42 +0100

    fix the default value for dexcription and made migrations

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, Listing, Category
+from .models import User, Category, Listing
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Listing)
 admin.site.register(Category)
+admin.site.register(Listing)
