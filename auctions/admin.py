189b839407edd07f6dd79f9fe9fa6bb3fc73e60a
Said <baraoudev@gmail.com>
2024-12-30 19:23:03 +0100

    removed bid model and import in admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import User, Listing, Bid, Description, Category
+from .models import User, Listing, Description, Category
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Listing)
-admin.site.register(Bid)
 admin.site.register(Description)
 admin.site.register(Category)
--- auctions/models.py
+++ auctions/models.py
@@ -14,20 +14,6 @@ class Listing(models.Model):
     def __str__(self):
         return f"{self.title} Price: {self.price} {self.currency} {self.publication_date}"
 
-class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bid')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name='bid')
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_length=3, default='USD')
-    bid_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('-bid_time',) 
-        # Most recent bid first
-
-    def __str__(self):
-        return f"Bid of : {self.price} {self.currency} by {self.user.first_name}"
-
 class Description(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='description')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='description')
