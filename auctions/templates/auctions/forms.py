ae16ec37092b645eace31ec50845bb294097fa2d
Said <baraoudev@gmail.com>
2025-01-03 09:26:10 +0100

    add widgets to meta class in add listing form

--- auctions/templates/auctions/forms.py
+++ auctions/templates/auctions/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+
 from .models import Listing
 
 class AddListingForm(forms.ModelForm):
   class Meta:
     model = Listing
     fields = ('title', 'description', 'price', 'image_url', 'category',)
+
+    widgets = {
+      'title': forms.TextInput(),
+      'description': forms.Textarea(),
+      'price': forms.NumberInput(),
+      'image_url': forms.TextInput(),
+      'category': forms.Select()
+    }
