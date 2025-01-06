7a1f73f547bdd21ecb1881b00ac66064f80dbda1
Said <baraoudev@gmail.com>
2025-01-06 05:33:54 +0100

    add simple form as_p and removed crispy form

--- commerce/settings.py
+++ commerce/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auctions',
-    'crispy_forms',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
