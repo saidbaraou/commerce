d60f9e6627bfe30d4162e3393d1c220cd8b23311
Said <baraoudev@gmail.com>
2025-01-10 11:09:52 +0100

    add style form

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,15 +2,27 @@ from django import forms
 
 from .models import Listing
 
+INPUT_CLASSES = 'form-control w-60 rounded py-2 px-3 birder'
+
 class AddListingForm(forms.ModelForm):
   class Meta:
     model = Listing
     fields = ('title', 'description', 'price', 'image_url', 'category',)
 
     widgets = {
-      'title': forms.TextInput(),
-      'description': forms.Textarea(),
-      'price': forms.NumberInput(),
-      'image_url': forms.TextInput(),
-      'category': forms.Select()
+      'title': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'description': forms.Textarea(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'price': forms.NumberInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'image_url': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'category': forms.Select(attrs={
+        'class': INPUT_CLASSES
+      })
     }
