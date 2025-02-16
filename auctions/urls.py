a0c7c08b80909b7b9347cb1d8dbc5e32f5fdb666
Said <baraoudev@gmail.com>
2025-02-16 20:39:43 +0100

    add path for watchlist in urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create-listing", views.create_listing_view, name="create_listing"),
     path("filter-category", views.filter_category_view, name="filter_category")
+    path("watchlist", views.watchlist_view, name="watchlist")
 ]
