b4d473513293ff21f1a52489476995bb17919404
Said <baraoudev@gmail.com>
2025-02-17 19:37:45 +0100

    feat:add listing page and related path

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create-listing", views.create_listing_view, name="create_listing"),
     path("filter-category", views.filter_category_view, name="filter_category"),
-    path("watchlist", views.watchlist_view, name="watchlist")
+    path("listing", views.listing_page_view, name="listing_page")
+    path("watchlist", views.watchlist_view, name="watchlist"),
 ]
