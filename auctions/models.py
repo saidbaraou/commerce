8ae90606b2e0f6fc13e175728a840412ad9a2162
Said <baraoudev@gmail.com>
2025-03-22 20:20:40 +0100

    feat(bid): add bid model

--- auctions/models.py
+++ auctions/models.py
@@ -44,3 +44,14 @@ class Watchlist(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Watchlist"
 
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"""
+    {self.user.username}
+    bid ${self.amount} on {self.listing.title}
+    """
