197c52191129c4f89910825896b1575abc0665b5
Said <baraoudev@gmail.com>
2025-11-07 20:38:32 +0100

    fix(watchlist): Fix watchlist view
    - Kept the number of listings watched by the user that is logged
    - Display listings watched by all users

--- auctions/views.py
+++ auctions/views.py
@@ -177,11 +177,15 @@ def get_watchlist_length(user):
 def watchlist_view(request):
     user = request.user
     watchlist_listings = get_watchlist_listing(user)
-
     watchlist_number = get_watchlist_length(user)
+    all_watched_listings = Listing.objects.filter(watchlists__isnull=False).distinct()
+    watchlist_user = get_watchlist_listing(user)
+
     context = {
         "watchlist_listings": watchlist_listings,
-        "watchlist_number": watchlist_number
+        "watchlist_number": watchlist_number,
+        "all_watched_listings": all_watched_listings,
+        "watchlist_user": watchlist_user
     }
     return render(request, "auctions/watchlist.html", context)
 
