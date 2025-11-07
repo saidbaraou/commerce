41c326f3ff163b32850888f01c743410edcb17eb
Said <baraoudev@gmail.com>
2025-11-07 21:15:04 +0100

    --allow-empty

--- auctions/views.py
+++ auctions/views.py
@@ -179,13 +179,13 @@ def watchlist_view(request):
     watchlist_listings = get_watchlist_listing(user)
     watchlist_number = get_watchlist_length(user)
     all_watched_listings = Listing.objects.filter(watchlists__isnull=False).distinct()
-    watchlist_user = get_watchlist_listing(user)
+
+    
 
     context = {
         "watchlist_listings": watchlist_listings,
         "watchlist_number": watchlist_number,
         "all_watched_listings": all_watched_listings,
-        "watchlist_user": watchlist_user
     }
     return render(request, "auctions/watchlist.html", context)
 
